058208642c30cc1bee4fc6db172fe901d22fb886
gabs-coding <gsbiel62@gmail.com>
2024-03-25 13:41:06 -0400

    [README.md, codigo.ino] Arquivos iniciais adicionados.

---
+++ codigo.ino
@@ -0,0 +1,16 @@
+/** 
+ * Código arduino para desbloquear uma trava elétrica de mola.
+ * O projeto usará um cartão RFID MIFARE para que o desbloqueio da tranca seja feito.
+ */
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+void setup() {
+	
+}
+
+void loop() {
+	
+}
+

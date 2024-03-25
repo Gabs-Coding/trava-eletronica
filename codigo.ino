1ef0ebe44c852f2278a7a2164329ecb0d1f709f6
gabs-coding <gsbiel62@gmail.com>
2024-03-25 19:53:46 -0400

    [codigo.ino] MFRC522 instanciado, definições de pinos e lógica de leitura de cartão adicionados.

--- codigo.ino
+++ codigo.ino
@@ -6,11 +6,26 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
+const int CS_PIN = 10; // Atribuição do pino 10 ao SPI slave select input (Pin 24, NSS, active low);
+const int RPD_PIN = 9; // Atribuição do pino 0 ao pino de reset e power down input (Pin 6, NRSTPD, active low);
+const int ATIVACAO_TRAVA_PIN = 2; // Atribuição do pino 2 para abrir e fechar a trava;
+
 void setup() {
-	
+	MFRC522 leitor_cartao(CS_PIN, RPD_PIN);
+	Serial.begin(9600); // Iniciando a porta serial;
+	SPI.begin(); // Iniciando o SPI BUS;
+	leitor_cartao.PCD_Init(); // Iniciando a interface MFRC522
+	pinMode(ATIVACAO_TRAVA_PIN, OUTPUT); // Definindo o modo do pino ATIVACAO_TRAVA_PIN para saída;
 }
 
 void loop() {
+	/**
+	 * Esse if verifica se: 1 não há um cartão sem cadastro sendo lido; 2 não há um dados de um cartão sendo
+   * lidos.
+   */
+	if (!leitor_cartao.PICC_IsNewCardPresent() || !leitor_cartao.PICC_ReadCardSerial()) {
+		return;
+	}
 	
 }
 
